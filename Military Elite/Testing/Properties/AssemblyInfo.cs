d572cd2af356ea18bdc79ef7cdd2541ed9b0463f
Christian Nytra <ch.nytra.13@gmail.com>
2019-01-04 13:02:54 +0100

    .

---
+++ Military Elite/Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e160713-6944-41eb-8fd8-e13b5f153c8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Military Elite/Testing/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var num1 = 2;
+            var num2 = 3;
+            var expected = 5;
+
+            // Act
+            var testManager = new testManager();
+            var result = testManager.bla(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
